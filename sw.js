de76fea786019eef28a8bb8b9c6b43373a779a64
l2015 <1053778503@qq.com>
2026-04-10 22:19:01 +0800

    fix(sw): bump cache version to force reload index.html

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
  * Provides offline caching and enables "Add to Home Screen"
  */
 
-const CACHE_NAME = 'sysdiagnose-v0.3.1';
+const CACHE_NAME = 'sysdiagnose-v0.3.2';
 const ASSETS = [
   './',
   './index.html',
